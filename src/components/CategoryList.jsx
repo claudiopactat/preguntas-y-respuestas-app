c58d7ad71a411c5ee2e25d7b3b2f27170b88fd89
Claudio Pactat <claudio.pactat@gmail.com>
2024-09-25 18:06:21 -0300

    first commit

---
+++ src/components/CategoryList.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { imgs, categories } from "../data";
+import { CategoryCard } from "./CategoryCard";
+
+const [
+  imgCiencia,
+  imgDeportes,
+  imgFilosofia,
+  imgGeografia,
+  imgHistoria,
+  imgLiteratura,
+  imgTecnologia,
+] = imgs;
+
+export const CategoryList = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-4 mt-10">
+      {/* Category Link Ciencia */}
+      <CategoryCard
+        category={categories.ciencia}
+        src={imgCiencia}
+        alt={`Categoria ${categories.ciencia}`}
+        gradientColor="from-purple-500 to-pink-500"
+      ></CategoryCard>
+
+      {/* Category Link Deportes */}
+      <CategoryCard
+        category={categories.deportes}
+        src={imgDeportes}
+        alt={`Categoria ${categories.deportes}`}
+        gradientColor="from-lime-500 to-zinc-500"
+      ></CategoryCard>
+
+      {/* Category Link Filosofia */}
+      <CategoryCard
+        category={categories.filosofia}
+        src={imgFilosofia}
+        alt={`Categoria ${categories.filosofia}`}
+        gradientColor="from-cian-500 to-lime-200"
+      ></CategoryCard>
+
+      {/* Category Link Geografia */}
+      <CategoryCard
+        category={categories.geografia}
+        src={imgGeografia}
+        alt={`Categoria ${categories.geografia}`}
+        gradientColor="from-sky-500 to-indigo-900"
+      ></CategoryCard>
+
+      {/* Category Link Historia */}
+      <CategoryCard
+        category={categories.historia}
+        src={imgHistoria}
+        alt={`Categoria ${categories.historia}`}
+        gradientColor="from-amber-400 to-emerald-500"
+      ></CategoryCard>
+
+      {/* Category Link Literatura */}
+      <CategoryCard
+        category={categories.literatura}
+        src={imgLiteratura}
+        alt={`Categoria ${categories.literatura}`}
+        gradientColor="from-amber-500 to-pink-500"
+      ></CategoryCard>
+
+      {/* Category Link Tecnologia */}
+      <CategoryCard
+        category={categories.tecnologia}
+        src={imgTecnologia}
+        alt={`Categoria ${categories.tecnologia}`}
+        gradientColor="from-purple-500 to-rose-500"
+      ></CategoryCard>
+    </div>
+  );
+};
---
+++ src/pages/CategoryPage.jsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Question } from "../components/Question";
+import { questions, imgs } from "../data";
+
+// Funcion para barajar las preguntas de cada categoria y tambien reducirla al numero de 5
+const shuffleArray = (array) => {
+  const newArray = array.sort(() => Math.random() - 0.5);
+  return newArray.slice(0, 5);
+};
+
+export const CategoryPage = () => {
+  // Leer el parametro de la URL
+  const { category } = useParams();
+
+  // const [imgCategory] = imgs.filter(
+  //   (img) => img === `/src/assets/${category.toLowerCase()}.png`
+  // );
+
+  console.log(category);
+
+  const [imgCategory] = imgs.filter(
+    (img) => img === `/src/assets/${category.toLowerCase()}.png`
+  );
+
+  const [questionsFiltered, setQuestionsFiltered] = useState(
+    questions.filter((question) => question.category === category)
+  );
+
+  const [indexQuestion, setIndexQuestion] = useState(0);
+
+  const [activeQuiz, setActiveQuiz] = useState(false);
+
+  useEffect(() => {
+    const newQuestions = shuffleArray(questionsFiltered);
+    setQuestionsFiltered(newQuestions);
+  }, []);
+
+  return (
+    <div
+      className="container flex flex-col item-center justify-center gap-10"
+      style={{ height: "calc(100vh - 5rem)" }}
+    >
+      {activeQuiz ? (
+        <Question
+          filteredQuestion={questionsFiltered[indexQuestion]}
+          setIndexQuestion={setIndexQuestion}
+          indexQuestion={indexQuestion}
+          questionsFiltered={questionsFiltered}
+          setActiveQuiz={setActiveQuiz}
+        />
+      ) : (
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col p-5 gap-5">
+            <h1 className="text-3xl text-teal-900 text-center font-bold">
+              {category}
+            </h1>
+
+            <div className="flex justify-center items-center">
+              <img src={imgCategory} alt={category} className="w-72" />
+            </div>
+          </div>
+
+          <button
+            className="text-white bg-gray-900 py-2 rounded-lg font-bold px-5 transition-all
+          hover:bg-yellow-500 hover:text-gray-900 w-72"
+            onClick={() => setActiveQuiz(true)}
+          >
+            Iniciar
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
